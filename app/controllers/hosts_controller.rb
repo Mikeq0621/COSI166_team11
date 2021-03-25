463b9fc5379808a41e3de65661e785d2974fd431
jasmynej14 <jasmynej14@brandeis.edu>
2021-03-25 19:16:12 -0400

    basic host/user profile

--- app/controllers/hosts_controller.rb
+++ app/controllers/hosts_controller.rb
@@ -3,4 +3,9 @@ class HostsController < ApplicationController
     def index
         @hosts = Host.all
     end
+
+    def show
+        @host = Host.find(params[:id])
+        @listings = @host.listings
+    end
 end
