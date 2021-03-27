9109641f08db6362f9a8467c0f701668c3efedc6
jasmynej14 <jasmynej14@brandeis.edu>
2021-03-27 01:46:14 -0400

    user can rent space (add transaction)

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -5,10 +5,18 @@ class TransactionsController < ApplicationController
     end
 
     def new
-
+        @transaction = Transaction.new
     end
 
     def create
+        listing = Listing.find(params[:transaction][:listing_id])
+        user = User.find(params[:transaction][:user_id])
+        transaction = Transaction.new(host_id:listing.host.id,user_id:params[:transaction][:user_id],listing_id:listing.id,price:30.00,duration:20)
+        if transaction.save
+            redirect_to user
+        else
+            render 'new'
+        end
 
     end
 
