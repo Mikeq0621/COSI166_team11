93cb3e362b95673f42ca40a03b81d88b341bb0f2
SHI QIU <mikeq@brandeis.edu>
2021-03-27 15:35:44 -0400

    Add host signup/login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,21 +2,40 @@ class SessionsController < ApplicationController
   def new
   end
 
-  def create
+  def newhost
+  end
+
+  def create_user
     # Find the signed up user in DB
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      flash[:success] = 'Logged in' # Not quite right!
+      flash[:success] = 'User Logged in' 
       log_in user
-      redirect_to root_path
+      redirect_to user
     elsif user && (!user.authenticate(params[:session][:password]))
-      flash.now[:danger] = 'Invalid password' # Not quite right!
+      flash.now[:danger] = 'Invalid password' 
       render 'new'
     else
-      flash.now[:danger] = 'Invalid email/password combination' # Not quite right!
+      flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
     end
+  end
 
+
+  def create_host
+    # Find the signed up user in DB
+    host = Host.find_by(email: params[:session][:email].downcase)
+    if host && host.authenticate(params[:session][:password])
+      flash[:success] = 'Host Logged in'
+      host_log_in host
+      redirect_to host
+    elsif host && (!host.authenticate(params[:session][:password]))
+      flash.now[:danger] = 'Invalid password'
+      render 'newhost'
+    else
+      flash.now[:danger] = 'Invalid email/password combination'
+      render 'newhost'
+    end
   end
 
   def destroy
