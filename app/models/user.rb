dcd5fc2ef2f7e83191cb383305412b39c446113c
Federico Berlfein <federico@federicos-mbp.dyn.brandeis.edu>
2021-03-27 23:45:39 -0400

    Use databse in Map

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,5 +10,6 @@ class User < ApplicationRecord
     validates :email, presence: true, length: { maximum: 255 },
                       format: { with: VALID_EMAIL_REGEX },
                       uniqueness: { case_sensitive: false }
+    
     validates :phone_number , presence: true, length: { maximum: 10 }, format: { with: VALID_PHONE_NUMBER}
 end
