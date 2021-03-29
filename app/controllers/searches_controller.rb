9a76818473d97d3f66917d9b823ddd0b069216a6
Aichuk Tripura <living.being123@gmail.com>
2021-03-29 17:57:11 -0400

    Additionalfilters

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -6,9 +6,17 @@ class SearchesController < ApplicationController
     else 
       if params[:search_filter] == 'city'
         @results = Host.all.where("city LIKE :search", search: params[:search]) 
+      elsif params[:search_filter] == 'duration'
+        host_listings = Host.joins(:listings).where("hosts.id = listings.host_id")
+        @results = host_listings.all.where("duration < :search", search: params[:search])
+      elsif params[:search_filter] == 'box'
+        host_listings = Host.joins(:listings).where("hosts.id = listings.host_id")
+        @results = host_listings.all.where("space < :search", search: params[:search])
       else
         @results = Host.all.where("zip_code LIKE :search", search: params[:search]) 
       end
     end
   end
+
+  
 end
