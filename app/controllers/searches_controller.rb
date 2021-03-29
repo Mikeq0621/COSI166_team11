1ef6461c07628a3eaa098ac2efb6554bb4769209
Aichuk Tripura <living.being123@gmail.com>
2021-03-29 17:59:22 -0400

    Small change to filter

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -8,10 +8,10 @@ class SearchesController < ApplicationController
         @results = Host.all.where("city LIKE :search", search: params[:search]) 
       elsif params[:search_filter] == 'duration'
         host_listings = Host.joins(:listings).where("hosts.id = listings.host_id")
-        @results = host_listings.all.where("duration < :search", search: params[:search])
+        @results = host_listings.all.where("duration > :search", search: params[:search])
       elsif params[:search_filter] == 'box'
         host_listings = Host.joins(:listings).where("hosts.id = listings.host_id")
-        @results = host_listings.all.where("space < :search", search: params[:search])
+        @results = host_listings.all.where("space > :search", search: params[:search])
       else
         @results = Host.all.where("zip_code LIKE :search", search: params[:search]) 
       end
